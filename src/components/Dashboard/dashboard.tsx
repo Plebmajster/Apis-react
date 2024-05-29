f5c9cb3b84683a0d6682d3eead7b7aa182384796
Plebmajster <vrbickysimon@gmail.com>
2024-05-29 13:20:32 +0200

    prechod na dashboard

--- src/components/Dashboard/dashboard.tsx
+++ src/components/Dashboard/dashboard.tsx
@@ -1,16 +1,26 @@
 import React, { useEffect } from 'react';
+import './dashboard.scss';
 
 const Dashboard = () => {
   useEffect(() => {
     const isAuthenticated = localStorage.getItem('isAuthenticated') === 'true';
     if (!isAuthenticated) {
       window.location.href = '/';
-      
     }
   }, []);
 
+  const handleLogout = () => {
+    localStorage.setItem('isAuthenticated', 'false');
+    window.location.href = '/';
+  };
+
   return (
-    <div>Insane Dashboard</div>
+    <div className='dashboard-container'>
+      <h1>Insane Dashboard</h1>
+      <button className='logout-button' onClick={handleLogout}>
+        Logout
+      </button>
+    </div>
   );
 };
 
