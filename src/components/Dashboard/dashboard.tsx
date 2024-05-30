23b52391d779ed72b12223988b04d5e37a178bce
Plebmajster <vrbickysimon@gmail.com>
2024-05-30 09:47:00 +0200

    main page datetime

--- src/components/Dashboard/dashboard.tsx
+++ src/components/Dashboard/dashboard.tsx
@@ -1,12 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './dashboard.scss';
 
 const Dashboard = () => {
+  const [currentTime, setCurrentTime] = useState('');
+  const [currentDate, setCurrentDate] = useState('');
+
   useEffect(() => {
     const isAuthenticated = localStorage.getItem('isAuthenticated') === 'true';
     if (!isAuthenticated) {
       window.location.href = '/';
     }
+
+    const updateDateTime = () => {
+      const now = new Date();
+      const time = now.toLocaleTimeString();
+      const date = now.toLocaleDateString();
+
+      setCurrentTime(time);
+      setCurrentDate(date);
+    };
+
+    updateDateTime(); // Initial call to set the time and date immediately
+    const intervalId = setInterval(updateDateTime, 1000); // Update every second
+
+    return () => clearInterval(intervalId); // Cleanup interval on component unmount
   }, []);
 
   const handleLogout = () => {
@@ -15,11 +32,18 @@ const Dashboard = () => {
   };
 
   return (
-    <div className='dashboard-container'>
-      <h1>Insane Dashboard</h1>
-      <button className='logout-button' onClick={handleLogout}>
-        Logout
-      </button>
+    <div className='section_dash'>
+      <div className='dashboard_container'>
+        <button className='logout_button' onClick={handleLogout}>
+          Logout
+        </button>
+      </div>
+      <div className='section_vnutro'>
+        <div className='time_date'>
+          <p className='time'>{currentTime}</p>
+          <p className='date'>{currentDate} | <img className='pin' src='/obr/pin.png' alt='pin'></img></p>
+        </div>
+      </div>
     </div>
   );
 };
